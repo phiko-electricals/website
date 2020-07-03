13dda8b362c46767aec6679404405b7a71a6d833
Isaac Barnett Ofori <oforibarnett@gmail.com>
2020-07-03 08:03:35 +0000

    update product category

--- res/script/product.js
+++ res/script/product.js
@@ -1,15 +1,47 @@
 const productsArr = [
   {
-    category:"Bulb",
-    items:["Angle Lamp Holder","30W Samsung LED Light","30W Gold LED Bulb","2U Bulb","6W LED Bulb Round Surface","9W Round LED Bulb","12W Round LED Bulb","6W Conduit LED","12W conduit LED","16W Two Colour LED Surface","18+6W LED Bulb Surface","9W LED Surface","18+6W LED Surface","24W LED Surface Remote","250W Street Bulb","400W Street Bulb","150W LED Bulb 3 Eye","50W One Eye LED Compund Light","100W Platy LED Compound Light","105 Energy Saving Bulb"]
+    category:"Lighting",
+    items:["Angle Lamp Holder","30W Samsung LED Light","30W Gold LED Bulb","2U Bulb","6W LED Bulb Round Surface","9W Round LED Bulb","12W Round LED Bulb","6W Conduit LED","12W conduit LED","16W Two Colour LED Surface","18+6W LED Bulb Surface","9W LED Surface","18+6W LED Surface","24W LED Surface Remote","250W Street Bulb","400W Street Bulb","150W LED Bulb 3 Eye","50W One Eye LED Compund Light","100W Platy LED Compound Light","105 Energy Saving Bulb","Emergency Bulb","Two Colour Holder Bulb","Street Bulb Holder"]
   },
   {
     category:"Fans",
-    items:["Standard Long Blade Fan","Standard Short Blade Fan","Orent Long Blade","Orent Short Blade","Delta Fan Long Blade","Delta Fan Short Blade"]
+    items:["Standard Long Blade Fan","Standard Short Blade Fan","Orent Long Blade","Orent Short Blade","Delta Fan Long Blade","Delta Fan Short Blade","Conduit Fan Regulator","Surface Fan Regulator"]
   },
   {
     category:"Cables",
     items:["1.5m2 Cable Standard","2.5m2 Cable Standard","2.5m2 Cable Classic","1.5m2 Cable Classic","1.5m2 Cable Tropical","2.5m2 Cable Tropical","4m2 Cable tropical","4m2 Cable Brsan","2.5m2 Cable Brsan","1.5 Cable Brsan","TV Cable","10m Cable","16m2 Cable","Focus Flexible wire","Standard Flexible wire"]
+  },
+  {
+    category:"Electrical Switches",
+    items:["RCD Breaker 100A","RCD Breaker 63A","RCD Breaker 32A","2 Pole Breaker","Consumer Unit 2 Way","Consumer Unit 4 Way","Consumer Unit 6 Way","Consumer Unit 8 Way","Harvells 6 Way","Harvells 8 way","Harvells 12 Way S/W","Standard 6 Way S/W","Standard 12 Way S/w","2 Way Skytrust S/W","4 Way Skytrust S/W","8 Way Skytrust S/W","12 Way Skytrust S/W","AC Switch","1 Gang 2 Way","2 Gang S/W","3 Gang S/W","Three Phase 3 Pole - 32A","Three Phase 4 Pole - 32A","Three Phase 4 Pole -100A","Three Phase 4 Pole - 63A","32A 3ph Isolator","63A 3ph Isolator","32A Change Over","63A Change Over", "Knife Change Over 63A","Knife Change Over 32A","Knife Change Over 100A"]
+  },
+  {
+    category:"Clips",
+    items:["Tower Clip 6mm","Tower Clip 8mm","Tower Clip 10mm","Tower Clip 12mm","Tower Clip 22mm"]
+  },
+  {
+    category:"Sockets & Extensions",
+    items:["Ty Socket","13A Single Socket","155B Delta Extension","506 Extension","504 Extension","Multi Socket","13A Double Socket"]
+  },
+  {
+    category:"Conduit Materials",
+    items:["Patral Box 3x3","Patral Box 3x6","Conduit Box 3x3","Conduit Box 3x6","Circular Box","PVC Pipe White","PVC Pipe Ash"]
+  },
+  {
+    category:"Rice Cookers",
+    items:[,"1.8 Lts Rice Cooker","1.5 Lts Rice Cooker"]
+  },
+  {
+    category:"Earthing",
+    items:["4 Feet Earthrod Normal","6 Feet Earthrod Normal","4 Feet Pure Copper","6 Feet Pure Copper"]
+  },
+  {
+    category:"Uncategorized Products",
+    items:["Straight Bottom Fittings","Tester","Hanging Holder","Phono to Jack","Phono to Phono","250W Chock","400W Chock","Ceiling Rose","Star Screw","Photocell","6+2 Indonasis","8 Way Indonasis","8+2 Indonasis","Screew Drive Set 9 Pieces","Screew Drive Set 7 Pieces","Screew Drive Set 6 Pieces","LED Driver","13A Plugs","Insulation Tape","Paper Insulation Tape","Fishing tape / Draw Wire","Bender"]
+  },
+  {
+    category:"And many more +",
+    items:["Call to verify"]
   }
 ]
 
@@ -49,114 +81,3 @@ closeBtn.addEventListener('click', e => {
   mainWrapper.classList.remove("rm-wrap")
   showAll.scrollIntoView({behavior: "smooth", block: 'center', inline: 'center'});
 })
-
-
-// ,
-// {
-//   category:"Breakers",
-//   items:[]
-// },
-// {
-//   category:"Bulb",
-//   items:[]
-// },
-// {
-//   category:"Bulb",
-//   items:[]
-// },
-// {
-//   category:"Bulb",
-//   items:[]
-// },
-// {
-//   category:"Bulb",
-//   items:[]
-// },
-// {
-//   category:"Bulb",
-//   items:[]
-// },
-// Straight Bottom Fittings
-// 2 Way Skytrust S/W
-// 4 Way Skytrust S/W
-// 8 Way Skytrust S/W
-// 12 Way Skytrust S/W
-// 13A Double Socket
-// Consumer Unit 2 Way
-// Consumer Unit 4 Way
-// Consumer Unit 6 Way
-// Consumer Unit 8 Way
-// AC Switch
-// 1 Gang 2 Way
-// 2 Gang S/W
-// 3 Gang S/W
-// Ty Socket
-// RCD Breaker 100A
-// RCD Breaker 63A
-// RCD Breaker 32A
-// 13A Single Socket
-// 155B Delta Extension
-// 506 Extension
-// 504 Extension
-// Tower Clip 6mm
-// Tower Clip 8mm
-// Tower Clip 10mm
-// Tower Clip 12mm
-// Tower Clip 22mm
-// Conduit Fan Regulator
-// Surface Fan Regulator
-// Tester
-// Hanging Holder
-// 2 Pole Breaker
-// Three Phase 3 Pole - 32A
-// Three Phase 4 Pole - 32A
-// Three Phase 4 Pole -100A
-// Three Phase 4 Pole - 63A
-// Multi Socket
-// Phono to Jack
-// Phono to Phono
-// 250W Chock
-// 400W Chock
-// Street Bulb Holder
-// Emergency Bulb
-// Patral Box 3x3
-// Patral Box 3x6
-// Conduit Box 3x3
-// Conduit Box 3x6
-// Ceiling Rose
-// Star Screw
-// 1.8 Lts Rice Cooker
-// 1.5 Lts Rice Cooker
-// Photocell
-// 32A 3ph Isolator
-// 63A 3ph Isolator
-// 32A Change Over
-// 63A Change Over
-// Circular box
-// Harvells 6 Way
-// Harvells 8 way
-// Harvells 12 Way S/W
-// Standard 6 Way S/W
-// Standard 12 Way S/w
-// 6+2 Indonasis
-// 8 Way Indonasis
-// 8+2 Indonasis
-// 4 Feet Earthrod Normal
-// 6 Feet Earthrod Normal
-// 4 Feet Pure Copper
-// 6 Feet Pure Copper
-// PVC Pipe White
-// PVC Pipe Ash
-// Screew Drive Set 9 Pieces
-// Screew Drive Set 7 Pieces
-// Screew Drive Set 6 Pieces
-// Knife Change Over 63A
-// Knife Change Over 32A
-// Knife Change Over 100A
-// Two Colour Holder Bulb
-// LED Driver
-// 13A Plugs
-// Insulation Tape
-// Paper Insulation Tape
-// Fishing tape / Draw Wire
-// Bender
